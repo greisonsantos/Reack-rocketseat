ac2ba43b4ab94166113643b393463f1ef8f72e9e
greisonsantos <greisonsantos03@gmail.com>
2019-03-20 09:29:12 -0300

    header com style css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 
+import './style.css';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello Word</h1>
+const App=()=>(
+ 
+   <div className="App">
+        <Header/>
       </div>
-    );
-  }
-}
+   
+  );
 
-export default App;
+  export default App;
+
+
